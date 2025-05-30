a5309f6b770b38dc6920617cc3a3245a1b831e5a
Faisal Alrico <if23.faisalalrico@mhs.ubpkarawang.ac.id>
2025-05-30 23:07:26 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RentThis',
+  webDir: 'www'
+};
+
+export default config;
